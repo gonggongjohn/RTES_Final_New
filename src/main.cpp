c20e32ba18ca76515915ebc723c6a77753c018e3
Eugene <eugene871208@gmail.com>
2023-12-20 17:15:40 -0500

    main

--- src/main.cpp
+++ src/main.cpp
@@ -71,95 +71,163 @@ void readGyroData(int samples) {
     }
 }
 
-int main()
-{
-  // Deselect the device
-  cs = 1;
+void initializeSPI() {
+    cs = 1;
+    spi.format(8, 3); // 8 bit data, high steady state clock, second edge capture
+    spi.frequency(1000000); // 1MHz clock rate
+}
 
-  // Setup the spi for 8 bit data, high steady state clock,
-  // second edge capture, with a 1MHz clock rate
-  spi.format(8, 3);
-  spi.frequency(1000000);
-
-  while (true)
-  {
-    // Orange screen to indicate that the loop is about to begin
-    //lcd.Clear(LCD_COLOR_YELLOW);
-    lcd.DisplayStringAt(0, LINE(10), (uint8_t *)"Prepare to walk", CENTER_MODE);
-
+void displayCountdown() {
     for (int countdown = 5; countdown > 0; countdown--) {
-    char countdownText[20];
-    sprintf(countdownText, "Starting in %d...", countdown);
-    lcd.DisplayStringAt(0, LINE(7), (uint8_t *)countdownText, CENTER_MODE);
-    wait_s(1);
-  }
+        char countdownText[20];
+        sprintf(countdownText, "Starting in %d...", countdown);
+        lcd.DisplayStringAt(0, LINE(7), (uint8_t *)countdownText, CENTER_MODE);
+        wait_s(1);
+    }
+}
 
-    // Green to indicate that the loop is running
-    lcd.Clear(LCD_COLOR_WHITE);
-    lcd.DisplayStringAt(0, LINE(10), (uint8_t *)"Walk now", CENTER_MODE);
-
-    // Record gyroscope data 40 times
-    readGyroData(40);
-
-    // Display red to indicate the loop is stopped
-    lcd.Clear(LCD_COLOR_WHITE);
-    lcd.DisplayStringAt(0, LINE(10), (uint8_t *)"Stop now", CENTER_MODE);
-
-
-    wait_s(3);
-    printf("DONE");
-
-    // Calculate the average angular velocities
-    double avg_x = 0;
-    double avg_y = 0;
-    double avg_z = 0;
-    for (int i = 0; i < 40; i++)
-    {
-      avg_x += arr_x[i] / 40;
-      avg_y += arr_y[i] / 40;
-      avg_z += arr_y[i] / 40;
+void calculateAndDisplayResults() {
+    double avg_x = 0, avg_y = 0, avg_z = 0;
+    for (int i = 0; i < 40; i++) {
+        avg_x += arr_x[i] / 40;
+        avg_y += arr_y[i] / 40;
+        avg_z += arr_z[i] / 40;
     }
 
-    // printf("\nAngular X = %lf dps\n\n", avg_x);
-    // printf("\nAngular Y = %lf dps\n\n", avg_y);
-    printf("\nAngular Z = %lf dps\n", avg_z);
+    printf("\nAngular X = %lf dps\nAngular Y = %lf dps\nAngular Z = %lf dps\n", avg_x, avg_y, avg_z);
 
-    // Multiply with radius to convert from angular to linear velocity
     int radius = 40;
-    double x_lin = avg_x * radius;
-    double y_lin = avg_y * radius;
-    double z_lin = avg_z * radius;
+    double x_lin = avg_x * radius, y_lin = avg_y * radius, z_lin = avg_z * radius;
     printf("Linear Velocity is %lf\n\n\n", z_lin);
 
-
-    // Multiply the linear velocity with the duration of measurement
     int dur = 20;
-    double dist_x = x_lin * dur;
-    double dist_y = y_lin * dur;
-    double dist_z = z_lin * dur;
+    double dist_x = x_lin * dur, dist_y = y_lin * dur, dist_z = z_lin * dur;
 
-    lcd.Clear(LCD_COLOR_WHITE);
-
-    // Arrays to concatenate the distance
-    //Reference from MBED's demo on LCD
-    //https://os.mbed.com/teams/ST/code/DISCO-F429ZI_LCDTS_demo//file/4faee567c996/main.cpp/
-    uint8_t xdist[30];
-    uint8_t ydist[30];
-    uint8_t zdist[30];
-
+    uint8_t xdist[30], ydist[30], zdist[30];
     sprintf((char *)xdist, "Dist X = %.2f", dist_x);
     sprintf((char *)ydist, "Dist Y = %.2f", dist_y);
     sprintf((char *)zdist, "Distance = %.2f cm", dist_z);
 
-    // lcd.DisplayStringAt(0, LINE(5), (uint8_t *) xdist, CENTER_MODE);
-    // lcd.DisplayStringAt(0, LINE(6), (uint8_t *) ydist, CENTER_MODE);
-
-    // Display the distance in Z direction
+    lcd.Clear(LCD_COLOR_WHITE);
     lcd.DisplayStringAt(0, LINE(7), (uint8_t *)zdist, CENTER_MODE);
-
     printf("Distance is %lf\n\n\n", abs(dist_z));
-
-    // Wait for 10 seconds until the loop restarts
-    wait_s(10);
-  }
 }
+
+int main() {
+    initializeSPI();
+
+    while (true) {
+        lcd.DisplayStringAt(0, LINE(10), (uint8_t *)"Prepare to walk", CENTER_MODE);
+        displayCountdown();
+
+        lcd.Clear(LCD_COLOR_WHITE);
+        lcd.DisplayStringAt(0, LINE(10), (uint8_t *)"Walk now", CENTER_MODE);
+
+        // Ensure that 'spi' is an SPI object and 'cs' is a DigitalOut object
+        // '40' is the number of samples you want to read
+        readGyroData(40);
+
+        lcd.Clear(LCD_COLOR_WHITE);
+        lcd.DisplayStringAt(0, LINE(10), (uint8_t *)"Stop now", CENTER_MODE);
+        wait_s(3);
+        printf("DONE");
+
+        calculateAndDisplayResults();
+        wait_s(10); // Wait for 10 seconds until the loop restarts
+    }
+}
+
+
+
+// int main()
+// {
+//   // Deselect the device
+//   cs = 1;
+
+//   // Setup the spi for 8 bit data, high steady state clock,
+//   // second edge capture, with a 1MHz clock rate
+//   spi.format(8, 3);
+//   spi.frequency(1000000);
+
+//   while (true)
+//   {
+//     // Orange screen to indicate that the loop is about to begin
+//     //lcd.Clear(LCD_COLOR_YELLOW);
+//     lcd.DisplayStringAt(0, LINE(10), (uint8_t *)"Prepare to walk", CENTER_MODE);
+
+//     for (int countdown = 5; countdown > 0; countdown--) {
+//     char countdownText[20];
+//     sprintf(countdownText, "Starting in %d...", countdown);
+//     lcd.DisplayStringAt(0, LINE(7), (uint8_t *)countdownText, CENTER_MODE);
+//     wait_s(1);
+//   }
+
+//     // Green to indicate that the loop is running
+//     lcd.Clear(LCD_COLOR_WHITE);
+//     lcd.DisplayStringAt(0, LINE(10), (uint8_t *)"Walk now", CENTER_MODE);
+
+//     // Record gyroscope data 40 times
+//     readGyroData(40);
+
+//     // Display red to indicate the loop is stopped
+//     lcd.Clear(LCD_COLOR_WHITE);
+//     lcd.DisplayStringAt(0, LINE(10), (uint8_t *)"Stop now", CENTER_MODE);
+
+
+//     wait_s(3);
+//     printf("DONE");
+
+//     // Calculate the average angular velocities
+//     double avg_x = 0;
+//     double avg_y = 0;
+//     double avg_z = 0;
+//     for (int i = 0; i < 40; i++)
+//     {
+//       avg_x += arr_x[i] / 40;
+//       avg_y += arr_y[i] / 40;
+//       avg_z += arr_y[i] / 40;
+//     }
+
+//     // printf("\nAngular X = %lf dps\n\n", avg_x);
+//     // printf("\nAngular Y = %lf dps\n\n", avg_y);
+//     printf("\nAngular Z = %lf dps\n", avg_z);
+
+//     // Multiply with radius to convert from angular to linear velocity
+//     int radius = 40;
+//     double x_lin = avg_x * radius;
+//     double y_lin = avg_y * radius;
+//     double z_lin = avg_z * radius;
+//     printf("Linear Velocity is %lf\n\n\n", z_lin);
+
+
+//     // Multiply the linear velocity with the duration of measurement
+//     int dur = 20;
+//     double dist_x = x_lin * dur;
+//     double dist_y = y_lin * dur;
+//     double dist_z = z_lin * dur;
+
+//     lcd.Clear(LCD_COLOR_WHITE);
+
+//     // Arrays to concatenate the distance
+//     //Reference from MBED's demo on LCD
+//     //https://os.mbed.com/teams/ST/code/DISCO-F429ZI_LCDTS_demo//file/4faee567c996/main.cpp/
+//     uint8_t xdist[30];
+//     uint8_t ydist[30];
+//     uint8_t zdist[30];
+
+//     sprintf((char *)xdist, "Dist X = %.2f", dist_x);
+//     sprintf((char *)ydist, "Dist Y = %.2f", dist_y);
+//     sprintf((char *)zdist, "Distance = %.2f cm", dist_z);
+
+//     // lcd.DisplayStringAt(0, LINE(5), (uint8_t *) xdist, CENTER_MODE);
+//     // lcd.DisplayStringAt(0, LINE(6), (uint8_t *) ydist, CENTER_MODE);
+
+//     // Display the distance in Z direction
+//     lcd.DisplayStringAt(0, LINE(7), (uint8_t *)zdist, CENTER_MODE);
+
+//     printf("Distance is %lf\n\n\n", abs(dist_z));
+
+//     // Wait for 10 seconds until the loop restarts
+//     wait_s(10);
+//   }
+// }
